a2a97b22c3f25e3398c52d05fda3183b8dfd19e9
khi8194 <khi8194@gmail.com>
2024-10-25 10:41:40 +0900

    django 조건문

--- member_app/views.py
+++ member_app/views.py
@@ -9,25 +9,50 @@ def home(request):
 # member 화면 출력 응답 함수
 def members(request):
   members = Member.objects.all().values()
-  context = {'members':members}
+  context = {"members":members}
   return HttpResponse(loader.get_template('members.html').render(context, request))
 
-# member 상세 정보 템플릿 출력 응답 함수
+# 멤버 상세정보 템플릿 출력 응답 함수
 def details(request, id):
   member = Member.objects.get(id=id)
-  context = {'member':member}
+  context = {"member":member}
   return HttpResponse(loader.get_template('details.html').render(context, request))
 
 
+# # 테스트용 뷰1
 # def testing(request):
-#   template = loader.get_template('template.html')
-#   context = {
-#     'firstname' : 'Linus'
-#   }
+#   template = loader.get_template("template.html")
+#   return HttpResponse(template.render())
+
+# # 테스트용 뷰2
+# def testing(request):
+#   myMembers = Member.objects.all().values()
+#   template = loader.get_template("template.html")
+#   context = {"myMembers":myMembers}
+#   return HttpResponse(template.render(context,request))
+
+# # 테스트용 뷰3
+# def testing(request):
+#   myMembers = Member.objects.all().values()
+#   template = loader.get_template("template.html")
+#   context = {"greeting":1}
+#   return HttpResponse(template.render(context,request))
+
+# # 테스트용 뷰4
+# def testing(request):
+#   myMembers = Member.objects.all().values()
+#   template = loader.get_template("template.html")
+#   context = {"greeting":3}
+#   return HttpResponse(template.render(context,request))
+
+# # 테스트용 뷰5
+# def testing(request):
+#   template = loader.get_template("template.html")
+#   context = {"greeting":2}
 #   return HttpResponse(template.render(context, request))
+
+# 테스트용 뷰6
 def testing(request):
-  template = loader.get_template('template.html')
-  context = {
-    'firstname' : 'Linus'
-  }
+  template = loader.get_template("template.html")
+  context = {"greeting":1, "day":"Friday"}
   return HttpResponse(template.render(context, request))
